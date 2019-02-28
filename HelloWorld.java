b7ad8b8dd891cc7baea04aa724b7c056ed9d8415
Adam Cohen <47546485+AdamCohen0@users.noreply.github.com>
2019-02-28 12:57:22 -0500

    primized

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,13 @@
 public class HelloWorld {
+    
     public static void main(String[] args) {
         String message = "Hello, world!";
         System.out.println(message);
+    }
+
+    public static void divideOut(int a, int n) {
+       if (a % n == 0)
+           return 0;
+           return 1 + divideOut(a / n, n);
     }
 }
